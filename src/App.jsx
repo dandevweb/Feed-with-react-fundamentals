545b1f6e8cc67039cb646d9111d118f65c71d29f
dandevweb <dandevweb@gmail.com>
2023-08-08 19:15:57 -0300

    starting with react

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-function App() {
+import { Post } from "./Post"
 
+export function App() {
   return (
     <>
-     <h1>Hello world</h1>
+      <Post
+        author="Danilo"
+        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas error at voluptates reiciendis sapiente assumenda! Laboriosam, rerum ducimus adipisci iure vel blanditiis quod sunt, saepe soluta minus asperiores veritatis facere."
+      />
+      <Post
+        author="Gabriel"
+        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas error at voluptates reiciendis sapiente assumenda! Laboriosam, rerum ducimus adipisci iure vel blanditiis quod sunt, saepe soluta minus asperiores veritatis facere."
+      />
+      <Post />
+      <Post />
     </>
   )
 }
 
-export default App
+
---
+++ src/Post.jsx
@@ -0,0 +1,10 @@
+
+export function Post(props) {
+  return (
+    <>
+      <h2>{props.author}</h2>
+      <p>{props.content}</p>
+    </>
+  )
+
+}
